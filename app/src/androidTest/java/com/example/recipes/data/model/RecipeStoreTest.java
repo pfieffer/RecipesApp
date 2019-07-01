60e23434619b503d2ae4b7b55a573991ef777921
Ravi Garbuja Pun <cbpmgr@gmail.com>
2019-07-01 12:56:05 +0545

    test: Write test for RecipeStore
    Write test for RecipeStore which verifies that our RecipeStore class handles the null directory properly.

---
+++ app/src/androidTest/java/com/example/recipes/data/model/RecipeStoreTest.java
@@ -0,0 +1,21 @@
+package com.example.recipes.data.model;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RecipeStoreTest {
+
+    @Test
+    public void nullDirectory() {
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        RecipeStore store = new RecipeStore(context, null);
+        assertNotNull(store);
+        assertNotNull(store.recipes);
+        assertEquals(0, store.recipes.size());
+    }
+}
