278e821d366910ee1a371e07f42573def5dd78ab
Ravi Garbuja Pun <cbpmgr@gmail.com>
2019-07-01 12:18:13 +0545

    test: Write test for mixed recipe

--- app/src/test/java/com/example/recipes/data/model/RecipeTest.java
+++ app/src/test/java/com/example/recipes/data/model/RecipeTest.java
@@ -12,5 +12,24 @@ public class RecipeTest {
         InputStream inputStream = RecipeTest.class.getResourceAsStream("/recipes/water.txt");
         Recipe recipe = Recipe.readFromStream(inputStream);
         assertNotNull(recipe);
+        assertEquals("water", recipe.id);
+        assertEquals("Water", recipe.title);
+        assertEquals("Put glass under tap. Open tap. Close tap. Drink.", recipe.description);
+    }
+
+    @Test
+    public void mixed(){
+        InputStream inputStream = RecipeTest.class.getResourceAsStream("/recipes/mixed.txt");
+        Recipe recipe = Recipe.readFromStream(inputStream);
+        assertNotNull(recipe);
+        assertEquals("punch", recipe.id);
+        assertEquals("Punch", recipe.title);
+        assertEquals("Juice of 3 lemons\n" +
+                "1 orange\n" +
+                "1 pint grape juice\n" +
+                "1 cup sugar\n" +
+                "1 cup water\n" +
+                "1 pine apple juice\n" +
+                "Mix all together and strain. Add large piece of ice.", recipe.description);
     }
 }
