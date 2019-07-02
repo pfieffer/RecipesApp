ba6322df94b37a83891eb4f76ff7afeb1f544cba
Ravi Garbuja Pun <cbpmgr@gmail.com>
2019-07-02 14:47:10 +0545

    test: Setup a hermatic test environment using test doubles
    Just like a stunt double, a test double is setup (InMemoryFavorites for SharedPreferencesFavorites). A method is made to clear favorites and it is added to @Before and alreadyFavorite case is tested.

--- app/src/androidTest/java/com/example/recipes/ui/RecipeActivityTest.java
+++ app/src/androidTest/java/com/example/recipes/ui/RecipeActivityTest.java
@@ -2,14 +2,18 @@ package com.example.recipes.ui;
 
 import android.content.Intent;
 
+import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
 import com.example.recipes.R;
+import com.example.recipes.data.local.InMemoryFavorites;
+import com.example.recipes.injection.TestRecipeApplication;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
-
+import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
@@ -17,15 +21,25 @@ import static androidx.test.espresso.matcher.ViewMatchers.isSelected;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.core.IsNot.not;
-import static androidx.test.espresso.Espresso.onView;
 
 public class RecipeActivityTest {
+    private static final String CARROTS_ID = "creamed_carrots";
+
     @Rule
     public ActivityTestRule<RecipeActivity> activityTestRule =
             new ActivityTestRule<>(RecipeActivity.class, true, false);
 
+    private InMemoryFavorites favorites;
+
+    @Before
+    public void clearFavorites() {
+        TestRecipeApplication app = (TestRecipeApplication) InstrumentationRegistry.getInstrumentation().getTargetContext().getApplicationContext();
+        favorites = (InMemoryFavorites) app.getFavorites();
+        favorites.clear();
+    }
+
     @Test
-    public void recipeNotFound(){
+    public void recipeNotFound() {
         activityTestRule.launchActivity(null);
 
         onView(withId(R.id.description))
@@ -36,15 +50,29 @@ public class RecipeActivityTest {
     }
 
     @Test
-    public void clickToFavorite(){
-        Intent intent = new Intent();
-        intent.putExtra(RecipeActivity.KEY_ID, "creamed_carrots");
-        activityTestRule.launchActivity(intent);
+    public void clickToFavorite() {
+        launchRecipe(CARROTS_ID);
 
         onView(withId(R.id.title))
                 .check(matches(withText("Creamed Carrots")))
                 .check(matches(not(isSelected()))) //see if title's star is not selected
                 .perform(click()) //perform a click
                 .check(matches(isSelected())); //see if title's star is selected
+    }
+
+    @Test
+    public void alreadyFavorite(){
+        favorites.put(CARROTS_ID, true);
+
+        launchRecipe(CARROTS_ID);
+
+        onView(withId(R.id.title))
+                .check(matches(isSelected()));
+    }
+
+    private void launchRecipe(String id) {
+        Intent intent = new Intent();
+        intent.putExtra(RecipeActivity.KEY_ID, id);
+        activityTestRule.launchActivity(intent);
     }
 }
